eb20f3b8568547a79242c55fa7dd95fef9528d8c
Izdormu <billymosh@gmail.com>
2021-10-17 13:19:38 +0200

    change in services

--- marvel_starter/src/services/MarvelService.js
+++ marvel_starter/src/services/MarvelService.js
@@ -1,4 +1,9 @@
+
+
 class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    // ЗДЕСЬ БУДЕТ ВАШ КЛЮЧ, ЭТОТ КЛЮЧ МОЖЕТ НЕ РАБОТАТЬ
+    _apiKey = 'apikey=0abdf7b5020caec37bc77aeba2994916';
     getResource = async (url) => {
         let res = await fetch(url);
 
@@ -10,16 +15,24 @@ class MarvelService {
 }
 
 
-    getAllCharacters= () => {
-        return this.getResource('https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=210&apikey=0abdf7b5020caec37bc77aeba2994916'
-            );
+    getAllCharacters = () => {
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
     }
 
-    getCharacter= (id) => {
-        return this.getResource(`https://gateway.marvel.com:443/v1/public/characters/${id}?limit=9&offset=210&apikey=0abdf7b5020caec37bc77aeba2994916`
-            );
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
     }
 
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            desc: char.description,
+            thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+        }
+    }
 
 }
 export default MarvelService;
