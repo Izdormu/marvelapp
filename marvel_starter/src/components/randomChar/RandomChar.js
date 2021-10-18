66030ca5c2c1013a8c213f078c78c7770b16d002
Izdormu <billymosh@gmail.com>
2021-10-18 21:02:59 +0200

    added catch error load descr spinner for loading

--- marvel_starter/src/components/randomChar/RandomChar.js
+++ marvel_starter/src/components/randomChar/RandomChar.js
@@ -3,7 +3,9 @@ import MarvelService from '../../services/MarvelService';
 import './randomChar.scss';
 //import thor from '../../resources/img/thor.jpeg';
 import mjolnir from '../../resources/img/mjolnir.png';
- import react from 'react';
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessagge';
+
 
 class RandomChar extends Component {
 
@@ -13,45 +15,44 @@ class RandomChar extends Component {
 
     }
     state = {
-        char: {} //в стейт записываеться нужный обьект  с нашщей конструкцией
-
+        char: {}, //в стейт записываеться нужный обьект  с нашщей конструкцией
+        loading : true,
+        error : false,
     }
 
     marvelService = new MarvelService(); //записываем в метод функцию получения данных с базы
 
-    onChatLoaded = (char) => {
-        this.setState({char}) //конструкция для записывания персонажа в стейт
+    onCharLoaded = (char) => {
+        this.setState({char,loading:false}) //конструкция для записывания персонажа в стейт
+    }
+
+    onError = () => {
+        this.setState({
+            loading: false,
+            error :true
+        })
     }
 
     updateChar = () =>{  //получем случайного персонажа из базы данных
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
         this.marvelService
             .getCharacter(id)
-            .then(this.onChatLoaded
+            .then(this.onCharLoaded
             ) 
+            .catch(this.onError)
     }
     
     render() {
-        const {char:{name, description,thumbnail,homepage,wiki}} = this.state;//вытаскиваем данные
+        const {char,loading,error} = this.state;//вытаскиваем данные
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error) ? <View char={char}/> : null
+
         return (
             <div className="randomchar">
-                <div className="randomchar__block">
-                    <img src={thumbnail} alt="Random character" className="randomchar__img"/>
-                    <div className="randomchar__info">
-                        <p className="randomchar__name">{name}</p>
-                        <p className="randomchar__descr">
-                            {description}
-                        </p>
-                        <div className="randomchar__btns">
-                            <a href={homepage} className="button button__main">
-                                <div className="inner">homepage</div>
-                            </a>
-                            <a href={wiki} className="button button__secondary">
-                                <div className="inner">Wiki</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                {errorMessage}
+                {spinner}
+                {content}
                 <div className="randomchar__static">
                     <p className="randomchar__title">
                         Random character for today!<br/>
@@ -68,6 +69,29 @@ class RandomChar extends Component {
             </div>
         )
     }
+    }
+
+    const View = ({char}) => {
+        const {name, description,thumbnail,homepage,wiki} = char;
+        return(
+            <div className="randomchar__block">
+                    <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+                    <div className="randomchar__info">
+                        <p className="randomchar__name">{name}</p>
+                        <p className="randomchar__descr">
+                            {description}
+                        </p>
+                        <div className="randomchar__btns">
+                            <a href={homepage} className="button button__main">
+                                <div className="inner">homepage</div>
+                            </a>
+                            <a href={wiki} className="button button__secondary">
+                                <div className="inner">Wiki</div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+        )
     }
 
 export default RandomChar;
--- marvel_starter/src/services/MarvelService.js
+++ marvel_starter/src/services/MarvelService.js
@@ -4,7 +4,7 @@ class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     // ЗДЕСЬ БУДЕТ ВАШ КЛЮЧ, ЭТОТ КЛЮЧ МОЖЕТ НЕ РАБОТАТЬ
     _apiKey = 'apikey=0abdf7b5020caec37bc77aeba2994916';
-    getResource = async (url) => {
+    getResource = async (url) => {  //достаем ресурсы
         let res = await fetch(url);
 
         if (!res.ok) {
@@ -15,19 +15,20 @@ class MarvelService {
 }
 
 
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
-    }
+getAllCharacters = async () => {
+    const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    return res.data.results.map(this._transformCharacter);
+} //получаем всеъ персонажей и  проганяем  всех персонажей по нужной констуркции чтобы создать свой обьект со своим набором
 
-    getCharacter = async (id) => {
-        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
-        return this._transformCharacter(res.data.results[0]);
-    }
+getCharacter = async (id) => {
+    const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    return this._transformCharacter(res.data.results[0]);
+} //получаем одного персонажа и проганяем  по нужной констуркции чтобы создать свой обьект со своим набором свойств
 
     _transformCharacter = (char) => {
         return {
             name: char.name,
-            desc: char.description,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
             thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
             homepage: char.urls[0].url,
             wiki: char.urls[1].url,
